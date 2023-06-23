64910e58719ed5ad9d5290bcd274de46ab7ee8cb
kyle Joest <kjoest92@gmail.com>
2023-06-23 16:48:44 -0500

    Finished JobTracker Data

--- JobTrackerApp.Data/IdentityModels.cs
+++ JobTrackerApp.Data/IdentityModels.cs
@@ -1,10 +1,13 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using JobTrackerApp.Data.JobApplication;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace JobTrackerApp.WebMVC.Models
+namespace JobTrackerApp.WebMVC.Data
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
@@ -28,6 +31,36 @@ namespace JobTrackerApp.WebMVC.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public DbSet<JobApplication> JobApplications { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+    }
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
         }
     }
 }
---
+++ JobTrackerApp.Data/JobApplication/JobApplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTrackerApp.Data.JobApplication
+{
+    public class JobApplication
+    {
+        public int JobApplicationId { get; set; }
+        public Guid OwnerId { get; set; }
+        public string JobTitle { get; set; }
+        public string CompanyName { get; set; }
+        public string ApplicationStatus { get; set; }
+        public string ApplicationMethod { get; set; }
+        public string ApplicationMaterials { get; set; }
+        public string ContactPerson { get; set; }
+        public string ContactInformation { get; set; }
+        public string ApplicationNotes { get; set; }
+
+        public DateTimeOffset ApplicationDate { get; set; }
+        public DateTimeOffset? ModifiedDate { get; set; }
+    }
+}
