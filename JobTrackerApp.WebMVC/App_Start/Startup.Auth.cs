fbda06772d93b987f9053e22748f9e5d56f0b848
kyle Joest <kjoest92@gmail.com>
2023-06-24 16:55:52 -0500

    Fixed some minor errors

--- JobTrackerApp.WebMVC/App_Start/Startup.Auth.cs
+++ JobTrackerApp.WebMVC/App_Start/Startup.Auth.cs
@@ -5,7 +5,7 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
-using JobTrackerApp.WebMVC.Models;
+using JobTrackerApp.WebMVC.Data;
 
 namespace JobTrackerApp.WebMVC
 {
--- JobTrackerApp.WebMVC/Controllers/AccountController.cs
+++ JobTrackerApp.WebMVC/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using JobTrackerApp.WebMVC.Models;
+using JobTrackerApp.WebMVC.Data;
 
 namespace JobTrackerApp.WebMVC.Controllers
 {
